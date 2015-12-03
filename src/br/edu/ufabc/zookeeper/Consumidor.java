0546b76912c0aafae6abd117ad838f1f03d56fd3
Newton Beck <newton.beck@gmail.com>
2015-12-03 10:04:15 -0200

    Finish!

--- src/br/edu/ufabc/zookeeper/Consumidor.java
+++ src/br/edu/ufabc/zookeeper/Consumidor.java
@@ -8,6 +8,7 @@ import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
 import org.apache.zookeeper.ZooDefs.Ids;
 
 import br.edu.ufabc.zookeeper.util.Nos;
@@ -41,6 +42,9 @@ public class Consumidor implements Runnable, Watcher {
 	public void process(WatchedEvent event) {
 		if (EventType.NodeChildrenChanged == event.getType()) {
 			System.out.println("Um novo item foi inserido na fila");
+
+			boolean tenhoLock = false;
+
 			try {
 				// Vai dormir antes de processar o nó
 				Thread.sleep(new Random().nextInt(3_000));
@@ -49,6 +53,7 @@ public class Consumidor implements Runnable, Watcher {
 
 				// Tenta pegar o lock
 				zooKeeper.create("/lock", new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+				tenhoLock = true;
 
 				System.out.println("Consegui o lock! :) Vamos trabalhar!");
 
@@ -62,17 +67,31 @@ public class Consumidor implements Runnable, Watcher {
 				System.out.println("Recebi o número: " + numero);
 
 				Thread.sleep(3_000);
-
-				System.out.println("Estou liberando o lock");
-
-				// Libera o lock
-				zooKeeper.delete("/lock", -1);
-
-				System.out.println("Lock liberado com sucesso");
 			} catch (KeeperException e) {
 				System.out.println("Não consegui pegar o lock, não foi dessa vez :(");
+
+				// Continua monitorando a fila
+				try {
+					zooKeeper.getChildren("/queue", true);
+				} catch (KeeperException | InterruptedException e1) {
+					// TODO O que fazer?
+				}
 			} catch (Exception e) {
 				// TODO O que fazer?
+			} finally {
+				// Libera o lock
+				if (tenhoLock) {
+					try {
+						System.out.println("Estou liberando o lock");
+
+						zooKeeper.delete("/lock", -1);
+
+						System.out.println("Lock liberado com sucesso");
+					} catch (Exception e) {
+						// TODO O que fazer?
+					}
+
+				}
 			}
 		}
 	}
