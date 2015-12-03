e138dcf68508cddfa472c15977427e7fd78c766d
Newton Beck <newton.beck@gmail.com>
2015-12-03 01:01:08 -0200

    Implementa a eleição de líder

--- src/br/edu/ufabc/zookeeper/Produtor.java
+++ src/br/edu/ufabc/zookeeper/Produtor.java
@@ -1,12 +1,16 @@
 package br.edu.ufabc.zookeeper;
 
-import java.io.IOException;
+import java.util.List;
 
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.Watcher.Event.EventType;
 import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
+
+import br.edu.ufabc.zookeeper.util.Nos;
 
 public class Produtor implements Runnable, Watcher {
 
@@ -16,11 +20,22 @@ public class Produtor implements Runnable, Watcher {
 
 	private ZooKeeper zooKeeper;
 
+	private String meuNo;
+
+	private boolean souLider;
+
 	public Produtor() throws Exception {
 		zooKeeper = new ZooKeeper("localhost", 3_000, this);
 
 		// Cria seu nó para leader election
-		zooKeeper.create("/leaders/node", new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+		meuNo = zooKeeper.create("/leaders/node", new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+		System.out.println("Nó " + meuNo + " para eleição criado com sucesso.");
+
+		// Registra o watch no diretório de leaders
+		List<String> nos = zooKeeper.getChildren("/leaders", true);
+
+		// Se não tem nenhum nó ele vira líder
+		souLider = nos.isEmpty();
 	}
 
 	@Override
@@ -32,7 +47,25 @@ public class Produtor implements Runnable, Watcher {
 
 	@Override
 	public void process(WatchedEvent event) {
-
+		if (EventType.NodeChildrenChanged == event.getType()) {
+			System.out.println("Iniciando a eleição...");
+			try {
+				// Espera até o nó antigo sair do diretório
+				Thread.sleep(3_000);
+				List<String> nos = zooKeeper.getChildren("/leaders", true);
+				String noMaisVelho = Nos.maisVelho(nos);
+				System.out.println("O nó mais velho é " + noMaisVelho);
+				if (meuNo.endsWith(noMaisVelho)) {
+					System.err.println("Venci a eleição! Chegou minha vez :)");
+					souLider = true;
+				} else {
+					System.out.println("Ainda não foi dessa vez :(");
+					souLider = false;
+				}
+			} catch (KeeperException | InterruptedException e) {
+				// TODO O que fazer?
+			}
+		}
 	}
 
 }
---
+++ src/br/edu/ufabc/zookeeper/util/Nos.java
@@ -0,0 +1,12 @@
+package br.edu.ufabc.zookeeper.util;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Nos {
+
+	public static String maisVelho(List<String> nos) {
+		return Collections.min(nos);
+	}
+
+}
