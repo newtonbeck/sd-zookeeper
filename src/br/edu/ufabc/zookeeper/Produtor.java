e24a9d6187fd4e073bc212d115ae26bb12063d50
Newton Beck <newton.beck@gmail.com>
2015-12-03 01:23:12 -0200

    Implementa a leitura dos números

--- src/br/edu/ufabc/zookeeper/Produtor.java
+++ src/br/edu/ufabc/zookeeper/Produtor.java
@@ -19,7 +19,7 @@ public class Produtor implements Runnable, Watcher {
 		new Thread(new Produtor()).start();
 	}
 
-	private Scanner scanner;
+	private Scanner input;
 
 	private ZooKeeper zooKeeper;
 
@@ -28,6 +28,9 @@ public class Produtor implements Runnable, Watcher {
 	private boolean souLider;
 
 	public Produtor() throws Exception {
+		// Cria o leitor do teclado
+		input = new Scanner(System.in);
+
 		// Conectar com o ZooKeeper
 		zooKeeper = new ZooKeeper("localhost", 3_000, this);
 
@@ -38,15 +41,34 @@ public class Produtor implements Runnable, Watcher {
 		// Registra o watch no diretório de leaders
 		List<String> nos = zooKeeper.getChildren("/leaders", true);
 
+		String maisVelho = Nos.maisVelho(nos);
+
 		// Se não tem nenhum nó ele vira líder
-		souLider = nos.isEmpty();
+		souLider = meuNo.endsWith(maisVelho);
 	}
 
 	@Override
 	public void run() {
 		while (true) {
 			if (souLider) {
+				// Lê o próximo número do usuário
+				System.out.println("Entre com um número positivo ou -1 para sair:");
+				int numero = input.nextInt();
 
+				// Se for -1 sai do programa
+				if (numero == -1) {
+					System.exit(0);
+					break;
+				}
+
+				// Caso contrário envia o número para a fila
+				try {
+					String noDoNumero = zooKeeper.create("/queue/node", String.valueOf(numero).getBytes(),
+							Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+					System.out.println("O número " + numero + " foi enviado para a fila dentro do nó " + noDoNumero);
+				} catch (KeeperException | InterruptedException e) {
+					// TODO O que fazer?
+				}
 			}
 		}
 	}
