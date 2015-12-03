62c6e6e7b0583e2745459f9f2ba8383344eae90e
Newton Beck <newton.beck@gmail.com>
2015-12-03 00:18:13 -0200

    Implementa a criação do nó de eleição

--- src/br/edu/ufabc/zookeeper/Produtor.java
+++ src/br/edu/ufabc/zookeeper/Produtor.java
@@ -1,9 +1,26 @@
 package br.edu.ufabc.zookeeper;
 
-public class Produtor implements Runnable {
+import java.io.IOException;
 
-	public static void main(String[] args) {
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
+
+public class Produtor implements Runnable, Watcher {
+
+	public static void main(String[] args) throws Exception {
 		new Thread(new Produtor()).start();
+	}
+
+	private ZooKeeper zooKeeper;
+
+	public Produtor() throws Exception {
+		zooKeeper = new ZooKeeper("localhost", 3_000, this);
+
+		// Cria seu nó para leader election
+		zooKeeper.create("/leaders/node", new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
 	}
 
 	@Override
@@ -11,6 +28,11 @@ public class Produtor implements Runnable {
 		while (true) {
 
 		}
+	}
+
+	@Override
+	public void process(WatchedEvent event) {
+
 	}
 
 }
